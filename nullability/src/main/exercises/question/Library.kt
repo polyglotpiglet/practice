e16a1449b8bc4074e56372f85e40f97b0b152302
polyglotpiglet <alexandra.p.batey@gmail.com>
2018-10-24 10:12:47 +0100

    tidy library

--- nullability/src/main/exercises/question/Library.kt
+++ nullability/src/main/exercises/question/Library.kt
@@ -1,30 +1,18 @@
 package question
 
 import library.*
-import java.util.ArrayList
 
 class Library(private val members: Members, private val catalogue: Catalogue, private val authors: Authors, private val loans: Loans) {
 
-    fun borrow(membershipNumber: Int, isbn: String): Loan? =
-            members.lookup(membershipNumber)?.let {
-                val memberHasTooManyBooks = loans.lentTo(it).size > 1
-                return if (memberHasTooManyBooks) null else lendBookToMember(isbn, it)
+    fun borrow(membershipNumber: Int, isbn: String): Loan? = members.lookup(membershipNumber)
+            ?.takeIf { member -> loans.lentTo(member).size < 2 }
+            ?.let { catalogue.lookup(isbn) }
+            ?.let { book -> loans.lend(membershipNumber, book) }
 
-            }
+    fun popularYoungAuthors() =
+            members.flatMap { member -> loans.lentTo(member) }
+                    .map { authors.lookup(it.book.author.id) }
+                    .filter { it?.takeIf { it.birthYear > 1995 } != null }
+                    .takeIf { !it.isEmpty() }
 
-    private fun lendBookToMember(isbn: String, member: Member): Loan? {
-        val book = catalogue.lookup(isbn)
-        return book?.let { loans.lend(member.id, book) }
-    }
-
-    fun popularYoungAuthors() = findYoungAuthors().let {
-        if (it.isEmpty()) null
-        else it
-    }
-
-    private fun findYoungAuthors(): List<Author> {
-        return members.flatMap { loans.lentTo(it) }
-                .map { authors.lookup(it.book.author.id) }
-                .filter { it?.let { it.birthYear > 1995 } ?: false }
-    }
 }
